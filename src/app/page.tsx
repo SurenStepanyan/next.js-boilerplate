940580bf450d293cda05b02e3b143d35d6d66d47
suren <suren.stepanyan@polymorphic.io>
2024-07-30 17:10:13 +0400

    Update (.eslintrc.json, tsconfig.json), created text-constants.ts, updated Page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
-import { Box, Heading } from '@chakra-ui/react';
+import { LIST_DATA, TextConstants } from '@/constants/text-constants';
+import { Box, Heading, List, ListIcon, ListItem } from '@chakra-ui/react';
+
+const { HEADING_TEXT, SUB_HEADING_TEXT } = TextConstants;
 
 function DefaultPage() {
   return (
@@ -6,10 +9,30 @@ function DefaultPage() {
       w="100%"
       h="100vh"
       display="flex"
+      flexDirection="column"
       justifyContent="center"
       alignItems="center"
     >
-      <Heading as="h1">Default Page</Heading>
+      <Heading as="h1">{HEADING_TEXT}</Heading>
+      <Heading mt="4" fontSize="24px">
+        {SUB_HEADING_TEXT}
+      </Heading>
+      <List mt="4" spacing="3">
+        {LIST_DATA.map((list) => (
+          <ListItem
+            key={list.id}
+            display="flex"
+            alignItems="center"
+            fontWeight="500"
+          >
+            <ListIcon></ListIcon>
+            {list.label} -
+            <Box as="span" fontWeight="300" fontSize="14px">
+              {list.additional_text}
+            </Box>
+          </ListItem>
+        ))}
+      </List>
     </Box>
   );
 }
